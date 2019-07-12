ba239062000f0e2f0a30cbe131a0cfbe2651bdee
ludwig_xtn <ludwig@extension-interactive.com>
2019-07-12 10:31:50 +0400

    Rock in progress...

--- chess.py
+++ chess.py
@@ -372,37 +372,31 @@ def Chessboard():
                     bg = 'true'
 
             if Plateau[ligne][colonne] is None:
-                if start == 'true':
-                    button[ligne][colonne] = tkinter.Button(Window, width=8, height=4, text='', bg=background, borderwidth=0).grid(row=ligne, column=colonne)
-                else:
-                    button[ligne][colonne]
+                Button(Window, width=8, height=4, text='', bg=background, borderwidth=0).grid(row=ligne, column=colonne)
             elif Plateau[ligne][colonne].name == 'Pawn' and Plateau[ligne][colonne].color == 'white':
-                if start == 'true':
-                    button[ligne][colonne] = tkinter.Button(Window, width=60, height=60, image=whitePawn, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
-                else:
-                    button[ligne][colonne]
+                Button(Window, width=60, height=60, image=whitePawn, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
             elif Plateau[ligne][colonne].name == 'Pawn' and Plateau[ligne][colonne].color == 'black':
-                button[ligne][colonne] = tkinter.Button(Window, width=60, height=60, image=blackPawn, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
+                Button(Window, width=60, height=60, image=blackPawn, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
             elif Plateau[ligne][colonne].name == 'Rook' and Plateau[ligne][colonne].color == 'white':
-                button[ligne][colonne] = tkinter.Button(Window, width=60, height=60, image=whiteRook, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
+                Button(Window, width=60, height=60, image=whiteRook, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
             elif Plateau[ligne][colonne].name == 'Rook' and Plateau[ligne][colonne].color == 'black':
-                button[ligne][colonne] = tkinter.Button(Window, width=60, height=60, image=blackRook, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
+                Button(Window, width=60, height=60, image=blackRook, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
             elif Plateau[ligne][colonne].name == 'Knight' and Plateau[ligne][colonne].color == 'white':
-                button[ligne][colonne] = tkinter.Button(Window, width=60, height=60, image=whiteKnight, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
+                Button(Window, width=60, height=60, image=whiteKnight, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
             elif Plateau[ligne][colonne].name == 'Knight' and Plateau[ligne][colonne].color == 'black':
-                button[ligne][colonne] = tkinter.Button(Window, width=60, height=60, image=blackKnight, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
+                Button(Window, width=60, height=60, image=blackKnight, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
             elif Plateau[ligne][colonne].name == 'Bishop' and Plateau[ligne][colonne].color == 'white':
-                button[ligne][colonne] = tkinter.Button(Window, width=60, height=60, image=whiteBishop, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
+                Button(Window, width=60, height=60, image=whiteBishop, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
             elif Plateau[ligne][colonne].name == 'Bishop' and Plateau[ligne][colonne].color == 'black':
-                button[ligne][colonne] = tkinter.Button(Window, width=60, height=60, image=blackBishop, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
+                Button(Window, width=60, height=60, image=blackBishop, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
             elif Plateau[ligne][colonne].name == 'Queen' and Plateau[ligne][colonne].color == 'white':
-                button[ligne][colonne] = tkinter.Button(Window, width=60, height=60, image=whiteQueen, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
+                Button(Window, width=60, height=60, image=whiteQueen, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
             elif Plateau[ligne][colonne].name == 'Queen' and Plateau[ligne][colonne].color == 'black':
-                button[ligne][colonne] = tkinter.Button(Window, width=60, height=60, image=blackQueen, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
+                Button(Window, width=60, height=60, image=blackQueen, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
             elif Plateau[ligne][colonne].name == 'King' and Plateau[ligne][colonne].color == 'white':
-                button[ligne][colonne] = tkinter.Button(Window, width=60, height=60, image=whiteKing, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
+                Button(Window, width=60, height=60, image=whiteKing, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
             elif Plateau[ligne][colonne].name == 'King' and Plateau[ligne][colonne].color == 'black':
-                button[ligne][colonne] = tkinter.Button(Window, width=60, height=60, image=blackKing, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
+                Button(Window, width=60, height=60, image=blackKing, bg=background, borderwidth=0).grid(row=ligne, column=colonne)
 
 
 def rightClick(event):
@@ -428,7 +422,7 @@ def Click(event):
 
         if Plateau[positionX][positionY] is not None:
             # highlight
-            #Button(Window, width=60, height=60, image=eval(''.join([PieceActivated.color, PieceActivated.name])), bg='gold', borderwidth=0).grid(row=positionX, column=positionY)
+            Button(Window, width=60, height=60, image=eval(''.join([PieceActivated.color, PieceActivated.name])), bg='gold', borderwidth=0).grid(row=positionX, column=positionY)
 
             print(Plateau[positionX][positionY].color, Plateau[positionX][positionY].name, 'at row:', positionX,'and column:', positionY)
             if Plateau[positionX][positionY].color == player:
@@ -463,7 +457,20 @@ def Click(event):
             print('***************************************')
             print('Is my King checked if I move like this?')
 
-            check()             
+            check()       
+
+        if bigRockInProgress == True:                                
+            # we temporary move the piece object on the new Plateau position
+            originalPiece = Plateau[positionX][positionY+1]
+            Plateau[positionX][positionY+1] = PieceActivated
+            # we temporary define the new position of the piece object
+            PieceActivated.row = positionX
+            PieceActivated.column = positionY+1
+        
+            print('***************************************')
+            print('Is my King checked if I move like this?')
+
+            check()          
 
         # we temporary remove old graphical position of the piece
         Plateau[PieceActivated.row][PieceActivated.column] = None
